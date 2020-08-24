83540f49ef989227916255682def8e1ed5ab9f55
JUkhan <jasim.uddin.khan@gmail.com>
2020-08-24 17:56:25 +0600

    testing

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-
+	//jwt auth
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
