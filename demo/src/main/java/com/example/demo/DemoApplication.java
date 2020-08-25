4055c68c26f70afd8b5564f9fc71a80fa4f6096e
JUkhan <jasim.uddin.khan@gmail.com>
2020-08-25 23:14:42 +0600

    intelliJ IDEA keyboard shortcut

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,21 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//      option+command+v  extract a var
+//		option+command+v  extract a method
+//		option+command+p  extract a method param
+//		option+command+n  inlining
+//		shift+control+f  search by word in files
+//		shift+shift  search by file name
+//		control+option+h  cal hierarchy
+//		command+w       select a word
+//		command+w +w      select a line
+//		command+y  remove current line
+//		command+d
+//		command+x
+//		command+c
+//		shift+command+t   generating test/if you apply this cmd from a test class then its navigate to the original class
+//      command+a
 
 
 @SpringBootApplication
